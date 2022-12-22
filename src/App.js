8250a0377b74184e7eb2bcb7bc633cc0d1008796
sofialeal95 <asleal95@gmail.com>
2022-12-22 09:47:57 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <a
-          href="https://github.com/sofialeal95/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>{" "}
-        by Ana Sofia Araiza
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/sofialeal95/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Ana Sofia Araiza
+        </footer>
+      </div>
     </div>
   );
 }
